faefba769ea08b7ce366a5aabcc3faba9e896f28
bohdan <bohdan@example.com>
2026-04-25 23:10:09 +0300

    pizza component

--- 03-pizza-menu/starter/src/App.js
+++ 03-pizza-menu/starter/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+
+  return (
+    <footer className="footer">
+      <h1>We are currently {isOpen ? "open" : "closed"}</h1>
+    </footer>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu:</h2>
+      <Pizza />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <>
+      <h3>{props.name}</h3>
+      <img src={props.photoName} alt="focaccia pizza" />
+      <p>{props.ingredients}</p>
+      <p>${props.price}</p>
+    </>
   );
 }
 
